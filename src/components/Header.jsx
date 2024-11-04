0dbc610c74bb067e04809813ecc5bd4b2bd64986
Dimex19 <azeezabolaji06gmail.com>
2024-11-04 09:12:35 +0100

    hero section fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,13 +36,18 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    
     margin: 20px 30px;
     height: 50px;
     padding: 20px;
-    /* margin-right: 30px; */
     /* border: 1px solid red; */
     background-color: #3f3c3b;
+
+    @media (max-width:450px){
+        width: 100vw;
+        margin: 0px;
+        padding: 10px;
+    }
+    
     `
 const Nav = styled.div`
     display: flex;
@@ -59,15 +64,29 @@ const NavLinks = styled.div`
         color: white;
         cursor: pointer;
         font-weight: 500;
-    }`
+    }
+    @media (max-width:450px){
+        display: none;
+    }
+    
+    `
 const Hamburger = styled.div`
     display: none;
     font-size: 32px;
-    margin-right: 20px;
+    /* margin-right: 20px; */
+    @media (max-width:450px){
+        display: block;
+    
+    }
+    
 `
 const NavIcons = styled.div`
     display: flex;
     justify-content: space-between;
     min-width: 100px;
     /* border: 1px solid red; */
+    @media (max-width:450px){
+        display: none;
+    }
+    
 `
---
+++ src/components/Hero.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import styled from 'styled-components'
+import Button from './Button'
+import HeroImage from '../assets/images/Me.png'
+
+const Hero = () => {
+    const btnStyle = {
+        backgroundColor: "#D4B134",
+        width: "200px",
+        height: "50px",
+        borderRadius: "10px",
+        border: "0px",
+        marginTop: "30px",
+        fontWeight: "500",
+        fontSize: "16px",
+        cursor: "pointer"
+    }
+
+    return (
+        <>
+            <Wrapper>
+                <HeroText>
+                    <h1 id='stack'>FULLSTACK ENGINEER</h1>
+                    <hr />
+                    <h1 id='myName'>AZEEZ ABOLAJI</h1>
+                    <p>Passionate Software Engineer with a focus on React Native development,
+                    dedicated to crafting elegant and user-friendly mobile applications.</p>
+                    <Button text="Contact me" style={btnStyle}/>
+                </HeroText>
+                <img src={HeroImage} alt="" />
+            </Wrapper>
+        </>
+)
+}
+
+export default Hero
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 90px);
+    color: #AAAAAA;
+    /* color: white; */
+    margin-left: 30px;
+    margin-right: 30px;
+    padding: 10px;
+    /* border: 1px solid red; */
+    /* color: #3f3c3b; */
+    /* min-height: calc(100vh - 0px); */
+    img {
+        height: 400px;
+        margin: auto;
+        /* border: 1px solid red; */
+        @media (max-width: 450px) {
+            display: none;
+        }
+    }
+    @media (max-width: 450px) {
+        flex-wrap: wrap;
+        margin-left: 10px;
+        margin-right: 10px;
+        padding: 0px;
+    }
+`
+const HeroText = styled.div`
+
+    #stack {
+        font-family: "Bebas Neue", sans-serif;
+        font-weight: 500;
+        font-style: normal;
+        font-size: 86px;
+        
+        @media (max-width: 450px){
+            line-height: 75px;
+            font-size: 74px;
+        }
+    }
+    #myName {
+        margin-top: 5px;
+        font-family: "Bebas Neue", sans-serif;
+        font-weight: 500;
+        font-style: normal;
+        font-size: 86px;
+        
+        @media (max-width: 450px){
+            line-height: 85px;
+            font-size: 74px;
+        }
+    }
+    p {
+        font-weight: 600;
+        }
+`
